11c0767d96cc81ef507676c8eaf990e18f7dd044
songweijia <songweijia@gmail.com>
2018-07-09 20:40:14 -0400

    Add default rdma configuration

--- derecho-dev/derecho.build.sh
+++ derecho-dev/derecho.build.sh
@@ -11,4 +11,13 @@ else
     cmake -DCMAKE_BUILD_TYPE=$1 ..
     make
     echo "Derecho $1 build completed successfully!"
+    # create default config
+    echo "fabric = sockets" >> rdma.cfg
+    echo "domain = ${domain}" >> rdma.cfg
+    echo "tx_depth = 256" >> rdma.cfg
+    echo "tx_depth = 256" >> rdma.cfg
+    cp rdma.cfg derecho/experiments/
+    cp rdma.cfg sst/experiments/
+    rm -f rdma.cfg
+
 fi
