7346b014e2b73b88a33a88a50e2e703d13bf9374
Weijia Song <songweijia@gmail.com>
2019-05-14 13:16:52 -0400

    updated to derecho master on May-14th 2019

--- derecho-dev/build-derecho.sh
+++ derecho-dev/build-derecho.sh
@@ -10,23 +10,32 @@ BRANCH=$2
 DEFAULT_COMMIT="HEAD"
 if [[ -z $BRANCH ]]; then
   BRANCH=master
-  DEFAULT_COMMIT="3eb02e646fe18db3548a49dc6e8153a2d5331481"
+  DEFAULT_COMMIT="fcb36cc66f4fba9b2e5ee405bc102eaf4f8273b1"
 fi
 NCPU=$3
 if [[ -z $NCPU ]]; then
-  NCPU=4
+  NCPU=`lscpu | grep ^CPU\(s\) | awk '{print $2}'`
 fi
 
 # step 1 clone
-git clone --recursive https://github.com/Derecho-Project/derecho-unified
-cd derecho-unified
+git clone --recursive https://github.com/Derecho-Project/derecho
+cd derecho
 echo "Using $BRANCH"
 git checkout -b $BRANCH
 git checkout $DEFAULT_COMMIT
-git submodule update --init
+
+# step 2 install dependencies
+cd scripts/prerequisites
+./install-mutils.sh
+./install-mutils-tasks.sh
+./install-mutils-containers.sh
+./install-spdlog.sh
+./install-libfabric.sh
+cd ../../
 
 # step 2 build
 mkdir $BUILD
 cd $BUILD
 cmake -DCMAKE_BUILD_TYPE=$BUILD ..
 make -j $NCPU
+make install
