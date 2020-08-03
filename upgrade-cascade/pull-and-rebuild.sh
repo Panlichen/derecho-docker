96b6522087b818bc3531d0792faebd04fb5cbd9f
Panlichen <plch368@qq.com>
2020-08-03 15:37:20 +0800

    pull and rebuild derecho and cascade

---
+++ upgrade-cascade/pull-and-rebuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INSTALL_PREFIX="/usr/local"
+
+# pull and rebuild cascade
+cd /cascade
+git pull
+
+if [ ! -d "build" ]; then
+  mkdir build
+else
+  rm -rf build && mkdir build
+fi
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} ..
+make -j `lscpu | grep "^CPU(" | awk '{print $2}'`
+make install
